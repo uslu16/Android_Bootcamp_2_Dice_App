39f571ec29088e8bafd746c62f75287a00a43b5b
uslu16 <36100420+uslu16@users.noreply.github.com>
2019-06-08 00:31:28 +0300

    onclick to be defined

--- app/src/main/java/com/example/android_bootcamp_2_dice_app/MainActivity.java
+++ app/src/main/java/com/example/android_bootcamp_2_dice_app/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.android_bootcamp_2_dice_app;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Context  context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button dice_roll;
+        ImageView dice_left;
+        ImageView dice_right;
+        ImageView dicee_logo;
+
+        context = this;
+
+        dice_left = findViewById(R.id.dice_left);
+        dice_right = findViewById(R.id.dice_right);
+        dicee_logo = findViewById(R.id.dicee_logo);
+
+        dice_roll= findViewById(R.id.dice_roll);
+
+        dice_roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(context, " Button Ckicl :)", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
+
 }
